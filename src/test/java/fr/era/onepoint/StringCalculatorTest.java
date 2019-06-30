003193cfdc26a16409c4ae58b62c95466e6cb650
Elie RASOANAIVO <el.rasoanaivo@gmail.com>
2019-06-30 11:00:20 +0200

    test: Ajout des cas de test simple. Pour une chaine vide, à un nombre, à deux nombres et à X nombres.

--- src/test/java/fr/era/onepoint/StringCalculatorTest.java
+++ src/test/java/fr/era/onepoint/StringCalculatorTest.java
@@ -4,18 +4,47 @@ import org.junit.Test;
 
 /**
  * Created by Elie RASOANAIVO on 30/06/2019.
- * @since 1.0
- * @version 1.0
  *
- * Classe de teste et exemple d'utilisation des fonctionnalités du calculateur de chaine de caractère.
+ * @version 1.0
+ *          <p>
+ *          Classe de teste et exemple d'utilisation des fonctionnalités du calculateur de chaine de caractère.
+ * @since 1.0
  */
 public class StringCalculatorTest {
+    private static final String CHAINE_VIDE = "";
+    private static final String CHAINE_SIMPLE_UN_ELEMENT = "3";
+    private static final String CHAINE_SIMPLE_DEUX_ELEMENTS = "1,4";
+    private static final String CHAINE_SIMPLE_X_ELEMENTS = "1,4,5,6,7";
+    private static final int RESULT_CHAINE_VIDE = 0;
+    private static final int RESULT_IMPLE_UN_ELEMENT = 3;
+    private static final int RESULT_SIMPLE_DEUX_ELEMENTS = 5;
+    private static final int RESULT_SIMPLE_X_ELEMENTS = 23;
 
     private StringCalculator stringCalculator = new StringCalculator();
 
 
     @Test(expected = IllegalArgumentException.class)
-    void calculerChaineNullTest() {
-       stringCalculator.add(null);
+    public void calculerChaineNullTest() {
+        stringCalculator.add(null);
+    }
+
+    @Test
+    public void calculerChaineVideTest() {
+        assert RESULT_CHAINE_VIDE == stringCalculator.add(CHAINE_VIDE);
+    }
+
+    @Test
+    public void calculerChaineAUnElementTest() {
+        assert RESULT_IMPLE_UN_ELEMENT == stringCalculator.add(CHAINE_SIMPLE_UN_ELEMENT);
+    }
+
+    @Test
+    public void calculerChaineADeuxElementsTest() {
+        assert RESULT_SIMPLE_DEUX_ELEMENTS == stringCalculator.add(CHAINE_SIMPLE_DEUX_ELEMENTS);
+    }
+
+    @Test
+    public void calculerChaineAXElementTest() {
+        assert RESULT_SIMPLE_X_ELEMENTS == stringCalculator.add(CHAINE_SIMPLE_X_ELEMENTS);
     }
 }
