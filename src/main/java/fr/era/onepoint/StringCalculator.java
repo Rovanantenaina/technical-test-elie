17bc03093fbe676c97fbf0b94e8901428ed35fc7
Elie RASOANAIVO <el.rasoanaivo@gmail.com>
2019-06-30 13:18:48 +0200

    feat: prise en compte des retours à la ligne comme délimiteur légale

--- src/main/java/fr/era/onepoint/StringCalculator.java
+++ src/main/java/fr/era/onepoint/StringCalculator.java
@@ -1,5 +1,6 @@
 package fr.era.onepoint;
 
+import java.text.MessageFormat;
 import java.util.Arrays;
 
 /**
@@ -13,7 +14,9 @@ import java.util.Arrays;
  */
 public class StringCalculator {
     private static final String EXCEPTION_PARAMETER_NON_NULL = "Le paramètre ne peut pas être NULL";
-    private static final String DEFAULT_DELIMITER = ",";
+    private static final String EXCEPTION_PARAMETER_MAL_FORMATER = "La chaine passé en paramètre est mal formater ou n''as pas une valeur numérique : {0}";
+    private static final String REGEX_BASE_DELIMITER = "[,\\n]";
+    private static final String REGEX_NOMBRE_SEULEMENT = "\\d";
 
     int add(String nombres) {
         if (null == nombres) throw new IllegalArgumentException(EXCEPTION_PARAMETER_NON_NULL);
@@ -22,8 +25,18 @@ public class StringCalculator {
     }
 
     private int getSommeNombre(String nombres) {
-        return Arrays.stream(nombres.split(DEFAULT_DELIMITER))
+        return Arrays.stream(getListeNombreStream(nombres))
+                .filter((String nombre) -> {
+                    if (!nombre.matches(REGEX_NOMBRE_SEULEMENT)) {
+                        throw new IllegalArgumentException(MessageFormat.format(EXCEPTION_PARAMETER_MAL_FORMATER, nombre));
+                    }
+                    return true;
+                })
                 .mapToInt(Integer::valueOf)
                 .sum();
+    }
+
+    private String[] getListeNombreStream(String nombres) {
+        return nombres.split(REGEX_BASE_DELIMITER);
     }
 }
